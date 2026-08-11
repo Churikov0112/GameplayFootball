398eac682bcc7ed6a385eb882cb959e33c66b367
politecat <churikov.egor@bk.ru>
2026-08-12 02:09:57 +0300

    feat(input): menu navigation from both stick and dpad

--- src/utils/gui2/guitask.cpp
+++ src/utils/gui2/guitask.cpp
@@ -170,6 +170,11 @@ namespace blunted {
 
         // needs windowing event?
         Vector3 stick1Direction = Vector3(axes[0], axes[1], 0);
+        // digital cross adds to stick for menu navigation (semantic dpad buttons)
+        if (UserEventManager::GetInstance().GetJoyButtonState(j, SDL_GAMEPAD_BUTTON_DPAD_UP))    stick1Direction.coords[1] += 1.0f;
+        if (UserEventManager::GetInstance().GetJoyButtonState(j, SDL_GAMEPAD_BUTTON_DPAD_DOWN))  stick1Direction.coords[1] -= 1.0f;
+        if (UserEventManager::GetInstance().GetJoyButtonState(j, SDL_GAMEPAD_BUTTON_DPAD_LEFT))  stick1Direction.coords[0] -= 1.0f;
+        if (UserEventManager::GetInstance().GetJoyButtonState(j, SDL_GAMEPAD_BUTTON_DPAD_RIGHT)) stick1Direction.coords[0] += 1.0f;
         if (j == GetActiveJoystickID() && stick1Direction.GetLength() != 0) {
           wEvent->SetDirection(stick1Direction);
           needsWindowingEvent = true;
